526fb216c8bba80b072b4659c162dbe5b39579d4
Bram Vanroy <2779410+BramVanroy@users.noreply.github.com>
2023-07-16 10:33:40 +0200

    FIX: remove post init

--- run_clm.py
+++ run_clm.py
@@ -235,20 +235,6 @@ class DataTrainingArguments:
         metadata={"help": "Denote how much the evaluation metric must improve to satisfy early stopping conditions."},
     )
 
-    def __post_init__(self):
-        if self.streaming:
-            require_version("datasets>=2.0.0", "The streaming feature requires `datasets>=2.0.0`")
-
-        if self.dataset_name is None and self.train_file is None and self.validation_file is None:
-            raise ValueError("Need either a dataset name or a training/validation file.")
-        else:
-            if self.train_file is not None:
-                extension = self.train_file.split(".")[-1]
-                assert extension in ["csv", "json", "txt"], "`train_file` should be a csv, a json or a txt file."
-            if self.validation_file is not None:
-                extension = self.validation_file.split(".")[-1]
-                assert extension in ["csv", "json", "txt"], "`validation_file` should be a csv, a json or a txt file."
-
 
 def main():
     # See https://gist.github.com/BramVanroy/f78530673b1437ed0d6be7c61cdbdd7c
